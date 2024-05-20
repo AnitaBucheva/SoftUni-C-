5c04321695507181921cda1b1a91eda3c1563ee1
Anita Bucheva <anitabucheva@abv.bg>
2024-05-20 13:53:05 +0300

    Create Program.cs

---
+++ Lab-Visual-Studio-01/Person Info/Program.cs
@@ -0,0 +1,7 @@
+﻿string firstname = Console.ReadLine();
+string lastname = Console.ReadLine();
+
+string country = Console.ReadLine();
+string town = Console.ReadLine();
+
+Console.WriteLine($"{firstname} {lastname} from {country} - {town}!");
