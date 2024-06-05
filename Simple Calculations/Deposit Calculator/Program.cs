4fa13e185743e4d9c8173791e1c283dbc6917146
Anita Bucheva <anitabucheva@abv.bg>
2024-06-05 14:05:31 +0300

    Create Program.cs

---
+++ Simple Calculations/Deposit Calculator/Program.cs
@@ -0,0 +1,7 @@
+﻿double deposit = double.Parse(Console.ReadLine());
+double interest = double.Parse(Console.ReadLine());
+int mount = int.Parse(Console.ReadLine());
+
+double mountlyInterest = deposit * interest / 100 / 12;
+double totalAmount = deposit + mountlyInterest * mount;
+Console.WriteLine($"{totalAmount:F2}");
