96000933b94b05bfa464164c4199d2d8841ac539
Anita Bucheva <anitabucheva@abv.bg>
2024-06-06 14:24:45 +0300

    Create Program.cs

---
+++ Simple Calculations/Mandatory Literature/Program.cs
@@ -0,0 +1,8 @@
+﻿int pages = int.Parse(Console.ReadLine());
+int pagesInHours = int.Parse(Console.ReadLine());
+int days = int.Parse(Console.ReadLine());
+
+int totalReadingTime = pages / pagesInHours;
+int hoursPerDay = totalReadingTime /days;
+
+Console.WriteLine(hoursPerDay);
