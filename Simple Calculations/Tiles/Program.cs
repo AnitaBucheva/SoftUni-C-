811e38f60916fc41561a5f16d060647e34ebc052
Anita Bucheva <anitabucheva@abv.bg>
2024-06-09 14:13:53 +0300

    Create Program.cs

---
+++ Simple Calculations/Tiles/Program.cs
@@ -0,0 +1,12 @@
+﻿double bathroomWidth = double.Parse(Console.ReadLine());
+double bathroomHeigh = double.Parse(Console.ReadLine());
+double tileWidth = double.Parse(Console.ReadLine());
+double tileHeigh = double.Parse(Console.ReadLine());
+
+double bathroomArea = bathroomWidth * bathroomHeigh;
+double surplus = bathroomArea * 0.1;
+double totalBath = bathroomArea + surplus;
+double tileArea = tileWidth * tileHeigh;
+
+double tilesNeeded = totalBath / tileArea;
+Console.WriteLine($"{tilesNeeded:F0}");
