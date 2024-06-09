73dd115fd7435abf8dbb5ccb8506707f3c142746
Anita Bucheva <anitabucheva@abv.bg>
2024-06-09 14:12:28 +0300

    Create Program.cs

---
+++ Simple Calculations/Supplies for School/Program.cs
@@ -0,0 +1,10 @@
+﻿int pens = int.Parse(Console.ReadLine());
+int markers = int.Parse(Console.ReadLine());
+int cleaner = int.Parse(Console.ReadLine());
+int discount = int.Parse(Console.ReadLine());
+
+double totalPrice = pens * 5.80 + markers * 7.20 + cleaner * 1.20;
+
+double discountAmount = totalPrice * discount / 100;
+double final = totalPrice - discountAmount;
+Console.WriteLine(final);
