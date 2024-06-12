dc4db91e52d5cdaf26aebb1547715f8c92df550d
Anita Bucheva <anitabucheva@abv.bg>
2024-06-12 14:07:11 +0300

    Create Program.cs

---
+++ Simple Conditional Statements/AreaOfFigures/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Read figure type from the console
+        string figureType = Console.ReadLine();
+
+        // Declare variables for dimensions and area
+        double dimension1, dimension2, area;
+
+        // Calculate area based on figure type
+        switch (figureType.ToLower())
+        {
+            case "square":
+                dimension1 = double.Parse(Console.ReadLine());
+                area = dimension1 * dimension1;
+                break;
+            case "rectangle":
+                dimension1 = double.Parse(Console.ReadLine());
+                dimension2 = double.Parse(Console.ReadLine());
+                area = dimension1 * dimension2;
+                break;
+            case "circle":
+                dimension1 = double.Parse(Console.ReadLine());
+                area = Math.PI * dimension1 * dimension1;
+                break;
+            default:
+                Console.WriteLine("Invalid figure type!");
+                return;
+        }
+
+        // Print the calculated area formatted to the 2nd decimal place
+        Console.WriteLine($"{area:F2}");
+    }
+}
