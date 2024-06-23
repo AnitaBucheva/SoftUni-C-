326ca0c0bbff7fc47f2ce55155af0a6d4df40af7
Anita Bucheva <anitabucheva@abv.bg>
2024-06-23 13:55:32 +0300

    Create Program.cs

---
+++ Simple Conditional Statements/ValidTriangle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Read sides of the triangle from the console
+        int side1 = int.Parse(Console.ReadLine());
+        int side2 = int.Parse(Console.ReadLine());
+        int side3 = int.Parse(Console.ReadLine());
+
+        // Check if the triangle is valid
+        bool isValidTriangle = (side1 < side2 + side3) && (side2 < side1 + side3) && (side3 < side1 + side2);
+
+        // Print the result
+        if (isValidTriangle)
+        {
+            Console.WriteLine("Valid Triangle");
+        }
+        else
+        {
+            Console.WriteLine("Invalid Triangle");
+        }
+    }
+}
