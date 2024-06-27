98430eb0b80bd0d0cd03b4d90874129b33e297d3
Anita Bucheva <anitabucheva@abv.bg>
2024-06-27 13:56:43 +0300

    Create Program.cs

---
+++ Simple and Complex Conditional Statements/VacationExpenses/Program.cs
@@ -0,0 +1,56 @@
+﻿string season = Console.ReadLine();
+string accommodationType = Console.ReadLine();
+int daysCount = int.Parse(Console.ReadLine());
+double pricePerNight = 0;
+double discount = 0;
+
+if (season == "Spring")
+{
+    discount = 0.2;
+    if(accommodationType == "Hotel")
+    {
+       pricePerNight = 30;
+    }
+    else if(accommodationType == "Camping")
+    {
+        pricePerNight = 10;
+    }
+}
+else if (season == "Summer")
+{
+     if(accommodationType == "Hotel")
+    {
+        pricePerNight = 50;
+    }
+     else if(accommodationType == "Camping")
+    {
+        pricePerNight = 30;
+    }
+}
+else if(season == "Autumn")
+{
+    discount = 0.3;
+    if(accommodationType == "Hotel")
+    {
+        pricePerNight = 20;
+    }
+    else if(accommodationType == "Camping")
+    {
+        pricePerNight = 15;
+    }
+}
+else if(season == "Winter")
+{
+    discount = 0.1;
+    if(accommodationType == "Hotel")
+    {
+        pricePerNight = 40;
+
+    }else if(accommodationType == "Camping")
+    {
+        pricePerNight = 10;
+    }
+}
+double totalPriceNoDiscount = pricePerNight * daysCount;
+double totalPrice = totalPriceNoDiscount - (totalPriceNoDiscount * discount);
+Console.WriteLine($"{totalPrice:F2}");
