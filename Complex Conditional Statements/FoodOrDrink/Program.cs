005bce58f9393c1f442e5244a8168fb72faf1786
Anita Bucheva <anitabucheva@abv.bg>
2024-07-03 14:10:31 +0300

    Create Program.cs

---
+++ Complex Conditional Statements/FoodOrDrink/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Read the product from the console
+    
+        string product = Console.ReadLine().ToLower();
+
+        // Determine and print the type of the product
+        if (product == "curry" || product == "noodles" || product == "sushi" || product == "spaghetti" || product == "bread")
+        {
+            Console.WriteLine("food");
+        }
+        else if (product == "tea" || product == "water" || product == "coffee" || product == "juice")
+        {
+            Console.WriteLine("drink");
+        }
+        else
+        {
+            Console.WriteLine("unknown");
+        }
+    }
+}
