dfd996c7ec1309aaee8c519f8ada82e1c588b1b2
Anita Bucheva <anitabucheva@abv.bg>
2024-07-05 14:04:48 +0300

    Create Program.cs

---
+++ Complex Conditional Statements/LargestNumberOutofThree/Program.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Read three integer numbers from the console
+      
+        int num1 = int.Parse(Console.ReadLine());
+     
+        int num2 = int.Parse(Console.ReadLine());
+
+        int num3 = int.Parse(Console.ReadLine());
+
+        // Find the largest number
+        int largest = Math.Max(num1, Math.Max(num2, num3));
+
+        // Print the largest number
+        Console.WriteLine(largest);
+    }
+}
