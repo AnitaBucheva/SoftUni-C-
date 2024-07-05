16a1c71bce761f15d87f584e61c9a74ff35af0c2
Anita Bucheva <anitabucheva@abv.bg>
2024-07-05 14:03:52 +0300

    Create Program.cs

---
+++ Complex Conditional Statements/InvalidNumber/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Read an integer from the console
+        int number = int.Parse(Console.ReadLine());
+
+        // Check if the number is valid
+        if ((number < 100 || number > 200) && number != 0)
+        {
+            Console.WriteLine("invalid");
+        }
+    }
+}
