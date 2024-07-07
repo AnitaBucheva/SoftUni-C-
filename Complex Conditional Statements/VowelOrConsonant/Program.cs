2b8ede5779432fef4b51e70af2b81edd0aae0c35
Anita Bucheva <anitabucheva@abv.bg>
2024-07-07 10:14:18 +0300

    Create Program.cs

---
+++ Complex Conditional Statements/VowelOrConsonant/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Read a character from the console
+      
+        char letter = Console.ReadLine()[0];
+
+        // Check if the character is a vowel
+        if ("AEIOUaeiou".IndexOf(letter) >= 0)
+        {
+            Console.WriteLine("Vowel");
+        }
+        else if (char.IsLetter(letter))
+        {
+            Console.WriteLine("Consonant");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input");
+        }
+    }
+}
