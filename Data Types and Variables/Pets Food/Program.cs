9ef3e7900869521e5aa2a79d840b6832f9e1e07f
Anita Bucheva <anitabucheva@abv.bg>
2024-07-11 14:01:47 +0300

    Create Program.cs

---
+++ Data Types and Variables/Pets Food/Program.cs
@@ -0,0 +1,4 @@
+﻿int dogFood = int.Parse(Console.ReadLine());
+int catFood = int.Parse(Console.ReadLine());
+double allFood = dogFood * 2.50 + catFood * 4.00;
+Console.WriteLine($"{allFood:F2} lv.");
