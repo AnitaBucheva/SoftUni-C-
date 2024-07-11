d31b8e7e2baf50b78b723eff054ff550c64c9bf4
Anita Bucheva <anitabucheva@abv.bg>
2024-07-11 14:03:15 +0300

    Create Program.cs

---
+++ Data Types and Variables/Projects Creation/Program.cs
@@ -0,0 +1,5 @@
+﻿string architectureName = Console.ReadLine();
+int count = int.Parse(Console.ReadLine());
+
+int hourseCount = count * 3;
+Console.WriteLine($"The architect {architectureName} will need {hourseCount} hours to complete {count} project/s.");
