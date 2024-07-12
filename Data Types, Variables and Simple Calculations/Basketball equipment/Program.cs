c0d6c9fdb8a28c289535e798ef6af53fc7fbc8cb
Anita Bucheva <anitabucheva@abv.bg>
2024-07-12 14:26:23 +0300

    Create Program.cs

---
+++ Data Types, Variables and Simple Calculations/Basketball equipment/Program.cs
@@ -0,0 +1,9 @@
+﻿int annualFeeLv = int.Parse(Console.ReadLine());
+
+double sneakersPriceLv = annualFeeLv - (annualFeeLv * 0.4);
+double teemPriceLv = sneakersPriceLv - (sneakersPriceLv * 0.2);
+double ballPriceLv = (1.0 / 4) * teemPriceLv;
+double accsessoaries = (1.0 / 5) * ballPriceLv;
+
+double totalPrice = annualFeeLv + sneakersPriceLv + teemPriceLv + ballPriceLv + accsessoaries;
+Console.WriteLine(totalPrice);
