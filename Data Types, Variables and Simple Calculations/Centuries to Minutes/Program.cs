688932cdb90dd9aecd69f85e48f0b1ba3444cbfa
Anita Bucheva <anitabucheva@abv.bg>
2024-07-18 14:27:34 +0300

    Create Program.cs

---
+++ Data Types, Variables and Simple Calculations/Centuries to Minutes/Program.cs
@@ -0,0 +1,16 @@
+﻿int centuries = int.Parse(Console.ReadLine());
+
+// Conversion constants
+const int yearsPerCentury = 100;
+const double daysPerYear = 365.2422;
+const int hoursPerDay = 24;
+const int minutesPerHour = 60;
+
+// Perform conversions
+int years = centuries * yearsPerCentury;
+double days = (int)Math.Floor(years * daysPerYear);
+int hours = (int)days * hoursPerDay;
+int minutes = hours * minutesPerHour;
+
+// Print results
+Console.WriteLine($"{centuries} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
