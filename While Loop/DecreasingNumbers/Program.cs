d9f3d2b67cf8f30d25d21e17fd9affaf80e38b6d
Anita Bucheva <anitabucheva@abv.bg>
2024-08-11 14:44:30 +0300

    Create Program.cs

---
+++ While Loop/DecreasingNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Countdown
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt the user to enter an integer N
+           
+            int N = int.Parse(Console.ReadLine());
+
+            // Ensure N is greater than 1
+            if (N <= 1)
+            {
+             
+                return;
+            }
+
+            // Loop from N down to 1
+            while (N >= 1)
+            {
+                // Print the current value of N
+                Console.WriteLine(N);
+
+                // Decrement the counter variable
+                N--;
+            }
+        }
+    }
+}
