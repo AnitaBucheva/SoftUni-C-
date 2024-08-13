3203d79a370486e9fd045d89664df4ff627b3a49
Anita Bucheva <anitabucheva@abv.bg>
2024-08-13 14:31:33 +0300

    Create Program.cs

---
+++ While Loop/Numbers1to100/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PrintNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i =1; // Initialize the counter variable
+
+            // Loop while i is less than or equal to 100
+            while (i <= 100)
+            {
+                // Print the current value of i
+                Console.WriteLine(i);
+
+                // Increment the counter variable
+                i++;
+            }
+        }
+    }
+}
