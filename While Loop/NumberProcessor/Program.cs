461f29d0b5a8b1da6c189a392503268c4d2b4c63
Anita Bucheva <anitabucheva@abv.bg>
2024-08-13 14:29:45 +0300

    Create Program.cs

---
+++ While Loop/NumberProcessor/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace CommandProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read the initial number from the input
+          
+            int number = int.Parse(Console.ReadLine());
+
+            // Process commands until "End" command is received
+            while (true)
+            {
+                // Read the next command
+               
+                string command = Console.ReadLine();
+
+                // Process the command
+                switch (command)
+                {
+                    case "Inc":
+                        number++; // Increment the number
+                        break;
+                    case "Dec":
+                        number--; // Decrement the number
+                        break;
+                    case "End":
+                        // Print the final number and stop the program
+                        Console.WriteLine($"{number}");
+                        return;
+                    
+                }
+            }
+        }
+    }
+}
