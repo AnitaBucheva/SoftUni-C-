15e11469d0844a7d3e89e6157d689f1e999df146
Anita Bucheva <anitabucheva@abv.bg>
2024-08-17 15:14:04 +0300

    Create Program.cs

---
+++ While Loop/OddNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ReadOddNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+           
+            while (number % 2 == 0)
+            {
+                number = int.Parse(Console.ReadLine());
+            }
+
+            // Print the odd number
+            Console.WriteLine($"{number}");
+        }
+    }
+}
