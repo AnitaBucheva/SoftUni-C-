da4c4e51affc0621c86a81e543676c9c14d737e0
Anita Bucheva <anitabucheva@abv.bg>
2024-09-14 14:17:26 +0300

    Create Program.cs

---
+++ Methods/GreaterOfTwoValues/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string type = Console.ReadLine();
+        string firstValue = Console.ReadLine();
+        string secondValue = Console.ReadLine();
+
+        switch (type)
+        {
+            case "int":
+                int intFirst = int.Parse(firstValue);
+                int intSecond = int.Parse(secondValue);
+                Console.WriteLine(GetMax(intFirst, intSecond));
+                break;
+
+            case "char":
+                char charFirst = char.Parse(firstValue);
+                char charSecond = char.Parse(secondValue);
+                Console.WriteLine(GetMax(charFirst, charSecond));
+                break;
+
+            case "string":
+                Console.WriteLine(GetMax(firstValue, secondValue));
+                break;
+        }
+    }
+
+    static int GetMax(int a, int b)
+    {
+        return a > b ? a : b;
+    }
+
+    static char GetMax(char a, char b)
+    {
+        return a > b ? a : b;
+    }
+
+    static string GetMax(string a, string b)
+    {
+        return string.Compare(a, b) > 0 ? a : b;
+    }
+}
