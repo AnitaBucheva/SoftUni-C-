a3a369cbd79d4acbf69afe86db6179dbf4429c95
Anita Bucheva <anitabucheva@abv.bg>
2024-09-20 14:16:31 +0300

    Create Program.cs

---
+++ Methods/RepeatString/Program.cs
@@ -0,0 +1,14 @@
+﻿string text = Console.ReadLine();
+int count = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Repeat(text, count));
+
+static string Repeat(string text, int count)
+{
+    string result = "";
+    for (int i = 1; i <= count; i++)
+    {
+        result += text;
+    }
+    return result;
+}
