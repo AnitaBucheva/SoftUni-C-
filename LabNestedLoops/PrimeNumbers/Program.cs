992dd2332190f8dbc79edd3466c30dfdfb7c9a7c
Anita Bucheva <anitabucheva@abv.bg>
2024-09-24 14:06:48 +0300

    Create Program.cs

---
+++ LabNestedLoops/PrimeNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _08.PrimeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            for (int currentNumber = start; currentNumber <= end; currentNumber += 1)
+            {
+                bool isPrme = true;
+                int divider = 2;
+
+                while (divider < end)
+                {
+                    if (currentNumber == divider)
+                    {
+                        divider += 1;
+                        continue;
+                    }
+
+                    if (currentNumber % divider == 0)
+                    {
+                        isPrme = false;
+                        break;
+                    }
+
+                    divider += 1;
+                }
+
+                if (isPrme)
+                {
+                    Console.Write($"{currentNumber} ");
+                }
+            }
+        }
+    }
+}
