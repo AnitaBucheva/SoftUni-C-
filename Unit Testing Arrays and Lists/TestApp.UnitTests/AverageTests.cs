e464c78efa897bbe13b2b6a98a5f8e8db3edd6c5
Anita Bucheva <anitabucheva@abv.bg>
2024-09-26 14:27:31 +0300

    Create AverageTests.cs

---
+++ Unit Testing Arrays and Lists/TestApp.UnitTests/AverageTests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class AverageTests
+{
+   
+    [Test]
+    public void Test_CalculateAverage_InputHasOneElement_ShouldReturnSameElement()
+    {
+        // Arrange
+        int[] array = { 42 };
+
+        // Act
+        double result = Average.CalculateAverage(array);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(42));
+    }
+
+    [Test]
+    public void Test_CalculateAverage_InputHasPositiveIntegers_ShouldReturnCorrectAverage()
+    {
+        // Arrange
+        int[] array = { 1, 2, 3, 4, 5, 6 };
+
+        // Act
+        double result = Average.CalculateAverage(array);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(3.5));
+    }
+
+    [Test]
+    public void Test_CalculateAverage_InputHasNegativeIntegers_ShouldReturnCorrectAverage()
+    {
+        // Arrange
+        int[] array = { -1, -2, -3, -4, -5, -6 };
+
+        // Act
+        double result = Average.CalculateAverage(array);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(-3.5));
+    }
+
+    [Test]
+    public void Test_CalculateAverage_InputHasMixedIntegers_ShouldReturnCorrectAverage()
+    {
+        // Arrange
+        int[] array = { 3, -1, 4 };
+
+        // Act
+        double result = Average.CalculateAverage(array);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(2));
+    }
+}
