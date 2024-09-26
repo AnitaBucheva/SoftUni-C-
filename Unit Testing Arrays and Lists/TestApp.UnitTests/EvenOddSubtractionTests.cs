511fa7a80e07a5535607fb76c23d2c6017a7ad47
Anita Bucheva <anitabucheva@abv.bg>
2024-09-26 14:34:32 +0300

    Create EvenOddSubtractionTests.cs

---
+++ Unit Testing Arrays and Lists/TestApp.UnitTests/EvenOddSubtractionTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class EvenOddSubtractionTests
+{
+    [Test]
+    public void Test_FindDifference_InputIsEmpty_ShouldReturnZero()
+    {
+        // Arrange
+        int[] emptyArray = Array.Empty<int>();
+
+        // Act
+        int result = EvenOddSubtraction.FindDifference(emptyArray);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(0));
+    }
+
+   
+    [Test]
+    public void Test_FindDifference_InputHasOnlyEvenNumbers_ShouldReturnEvenSum()
+    {
+        // Arrange
+        int[] emptyArray = new[] { 2, 4, 6 };
+        // Act
+        int result = EvenOddSubtraction.FindDifference(emptyArray);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(12));
+    }
+
+    [Test]
+    public void Test_FindDifference_InputHasOnlyOddNumbers_ShouldReturnNegativeOddSum()
+    {
+        // Arrange
+        int[] emptyArray = new[] { 1, 3, 5 };
+        // Act
+        int result = EvenOddSubtraction.FindDifference(emptyArray);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(-9));
+    }
+
+    [Test]
+    public void Test_FindDifference_InputHasMixedNumbers_ShouldReturnDifference()
+    {
+        // Arrange
+        int[] emptyArray = new[] { 1, 2, 3, 4, 5, 6};
+        // Act
+        int result = EvenOddSubtraction.FindDifference(emptyArray);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(3));
+    }
+}
