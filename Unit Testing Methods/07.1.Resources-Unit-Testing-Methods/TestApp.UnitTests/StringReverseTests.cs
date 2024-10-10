1278b7e94edae39959eea0ae7b130f7c4210626b
Anita Bucheva <anitabucheva@abv.bg>
2024-10-10 14:33:53 +0300

    Create StringReverseTests.cs

---
+++ Unit Testing Methods/07.1.Resources-Unit-Testing-Methods/TestApp.UnitTests/StringReverseTests.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class StringReverseTests
+{
+    // TODO: finish test
+    [Test]
+    public void Test_Reverse_WhenGivenEmptyString_ReturnsEmptyString()
+    {
+        // Arrange
+        string input  = string.Empty;
+
+        string actual = StringReverse.Reverse(input);
+
+        // Assert
+        Assert.AreEqual("", actual);
+    }
+
+    [Test]
+    public void Test_Reverse_WhenGivenSingleCharacterString_ReturnsSameCharacter()
+    {
+        string input = "X";
+
+        // Act
+        string result = StringReverse.Reverse(input);
+
+        // Assert
+        Assert.AreEqual("X", result);
+        Assert.AreEqual(1, result.Length);
+    }
+
+    [Test]
+    public void Test_Reverse_WhenGivenNormalString_ReturnsReversedString()
+    {
+        string input = "XYZ";
+
+        // Act
+        string result = StringReverse.Reverse(input);
+
+        // Assert
+        Assert.AreEqual(input.Length, result.Length);
+        Assert.AreEqual("ZYX", result);
+    }
+}
