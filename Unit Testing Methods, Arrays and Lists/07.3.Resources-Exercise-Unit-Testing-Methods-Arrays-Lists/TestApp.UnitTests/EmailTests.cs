7f8f1da3a2784b1200f00869d531c1b72607b272
Anita Bucheva <anitabucheva@abv.bg>
2024-10-29 14:43:00 +0200

    Create EmailTests.cs

---
+++ Unit Testing Methods, Arrays and Lists/07.3.Resources-Exercise-Unit-Testing-Methods-Arrays-Lists/TestApp.UnitTests/EmailTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class EmailTests
+{
+ 
+    [Test]
+    public void Test_IsValidEmail_ValidEmail()
+    {
+      
+        // Act
+        bool result = Email.IsValidEmail("test@example.com");
+        // Assert
+        Assert.IsTrue(result);
+    }
+
+    [Test]
+    public void Test_IsValidEmail_InvalidEmail()
+    {
+        bool result = Email.IsValidEmail("invalid-email");
+        Assert.IsFalse(result);
+    }
+
+    [Test]
+    public void Test_IsValidEmail_NullInput()
+    {
+        bool result = Email.IsValidEmail(string.Empty);
+        Assert.IsFalse(result);
+    }
+}
