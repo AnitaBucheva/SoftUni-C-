b7f9d5f559cbf10599bca56d47063330f6580aa8
Anita Bucheva <anitabucheva@abv.bg>
2024-10-29 14:44:14 +0200

    Create PrimeFactorTests.cs

---
+++ Unit Testing Methods, Arrays and Lists/07.3.Resources-Exercise-Unit-Testing-Methods-Arrays-Lists/TestApp.UnitTests/PrimeFactorTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class PrimeFactorTests
+{
+    [Test]
+    public void Test_FindLargestPrimeFactor_PrimeNumber()
+    {
+        //Act
+        long result = PrimeFactor.FindLargestPrimeFactor(5);
+        //Assert
+        Assert.AreEqual(5, result);
+    }
+
+    [Test]
+    public void Test_FindLargestPrimeFactor_LargeNumber()
+    {
+        //Act
+        long result = PrimeFactor.FindLargestPrimeFactor(123456789123456789);
+        //Assert
+        Assert.AreEqual(52579, result);
+    }
+}
