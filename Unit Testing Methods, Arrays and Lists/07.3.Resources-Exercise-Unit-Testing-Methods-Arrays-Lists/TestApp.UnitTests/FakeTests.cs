63d67b85e014ca0fd124aa90591c93f7533a3b92
Anita Bucheva <anitabucheva@abv.bg>
2024-10-29 14:45:20 +0200

    Create FakeTests.cs

---
+++ Unit Testing Methods, Arrays and Lists/07.3.Resources-Exercise-Unit-Testing-Methods-Arrays-Lists/TestApp.UnitTests/FakeTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+
+using System;
+
+namespace TestApp.UnitTests;
+
+public class FakeTests
+{
+    [Test]
+    public void Test_RemoveStringNumbers_RemovesDigitsFromCharArray()
+    {
+        //Arrange
+        char[] input = { '0', '1', 'a', 'b' };
+        //Act
+        char[] result = Fake.RemoveStringNumbers(input);
+        //Assert
+        Assert.That(result, Is.EqualTo(new char[] {'a', 'b'}));
+     }
+
+    [Test]
+    public void Test_RemoveStringNumbers_NoDigitsInInput_ReturnsSameArray()
+    {
+        //Arrange
+        char[] input = { 'c', 'd', 'a', 'b' };
+        //Act
+        char[] result = Fake.RemoveStringNumbers(input);
+        //Assert
+        Assert.That(result, Is.EqualTo(input));
+    }
+
+    [Test]
+    public void Test_RemoveStringNumbers_EmptyArray_ReturnsEmptyArray()
+    {
+        //Arrange
+        char[] input = Array.Empty<char>();
+        //Act
+        char[] result = Fake.RemoveStringNumbers(input);
+        //Assert
+        Assert.That(result, Is.Empty);
+    }
+}
